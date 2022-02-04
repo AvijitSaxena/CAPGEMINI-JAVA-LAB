432437c5d15adb87f68ce0daff521a82fd0c8dbb
Avijit Saxena <98951803+AvijitSaxena@users.noreply.github.com>
2022-02-04 15:09:26 +0530

    Add files via upload

---
+++ Conditional1.java
@@ -0,0 +1,20 @@
+package com.xyz;
+
+public class Conditional1 {
+	public static void main(String[] args) {
+	    int n = 10;
+	    int m = 20;
+
+	    if (n>m && n>0) {
+	System.out.println("n is greater");
+	    }
+	    else {
+	System.out.println("m is greater");
+	    }
+
+	System.out.println("Feel Happy to get the correct Result");
+	  }
+
+	
+
+}
