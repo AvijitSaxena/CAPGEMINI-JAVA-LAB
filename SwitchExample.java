589d849f2e842cc8feefed37bb12708502e2a4e4
Avijit Saxena <98951803+AvijitSaxena@users.noreply.github.com>
2022-02-04 15:01:36 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,56 @@
+package com.xyz;
+
+public class SwitchExample{
+	public static void main(String args[]) {
+		int x = 3;
+        float y = 3.2f;   // Java considers any decimal number as a double value by default
+        
+        switch(x) {
+        
+        
+        
+        case 1: 
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+            
+        case 4:
+            System.out.println("Thursday");
+            break;
+            
+        case 5:
+            System.out.println("Friday");
+            break;
+            
+        case 6:
+            System.out.println("Saturday");
+            break;    
+        
+        case 7:
+            System.out.println("Sunday");
+            break;    
+        default:
+            System.out.println("default");
+           break;
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+}
+ 
+	
+	       
+	  
+
+
