3fdf1313c1705dc17fa259d839f4e777fd33cefc
Avijit Saxena <98951803+AvijitSaxena@users.noreply.github.com>
2022-02-07 15:42:33 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,12 @@
+package com.xyz;
+
+public class String3 {
+	public static void main(String[] args) {
+	    String str1 = "AvijitSaxena";
+	System.out.println(str1.substring(0, 7));
+    System.out.println(str1.substring(0, 5));
+	System.out.println(str1.substring(3, 5));
+	  }
+
+
+}
